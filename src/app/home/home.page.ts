ccfb0c36fef819b327d4266f1c7d516c4f8e195e
rtwohey <rtwohey@ionic.io>
2024-08-02 10:01:48 -0400

    added in-app-review plugin and button to test it

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { InAppReview } from '@capacitor-community/in-app-review';
 
 @Component({
   selector: 'app-home',
@@ -8,5 +9,14 @@ import { Component } from '@angular/core';
 export class HomePage {
 
   constructor() {}
+
+  async requestReview() {
+    try {
+      const result = await InAppReview.requestReview();
+      console.log('In App Review Requested:', result);
+    } catch (e) {
+      console.error('In App Review Request Failed:', e);
+    }
+  }
 
 }
