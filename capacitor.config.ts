f92870e50ce702333995c88cfafc0d34dcec3370
rtwohey <rtwohey@ionic.io>
2024-08-02 09:49:44 -0400

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'app-rate',
+  webDir: 'www'
+};
+
+export default config;
